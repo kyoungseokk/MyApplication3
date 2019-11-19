e2bfeae1e7b5c4f3f6c18613cfb9bf408cee331c
이경석 <kyoungseok@hanmail.net>
2019-11-19 22:23:29 +0900

    파일 받으세요

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+        textView.setText("안녕하세요");
+
+    }
+}
